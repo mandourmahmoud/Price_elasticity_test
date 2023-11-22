0eeb29886bd375b9a8dd14d35af48d1844f959ef
Mahmoud Mandour <mahmoudmandour@Mahmoud.Mandour MacBookPro>
2023-11-22 15:34:29 +0200

    Updating correct actual data query

--- acutal_data_script.sql
+++ acutal_data_script.sql
@@ -1,8 +1,17 @@
-select product_sku
-,total_preds
-,discount_12m
-,high_price_12m
-,active_price_12m
-,snapshot_date::date
-from pricing.price_elasticity_forecast
-where snapshot_date::date = '2023-10-30'
+select  dwh.product_sku
+,sum(dwh.completed_orders) total_submitted_orders
+--,dwh.fact_day
+--,dwh.store_id
+,AVG(CASE WHEN apgs.rental_plan_price_12_months LIKE '%,%'
+         THEN SPLIT_PART(apgs.rental_plan_price_12_months, ',', 1)::FLOAT
+         ELSE apgs.rental_plan_price_12_months::FLOAT
+    END) AS avg_m12_low_price
+,AVG(CASE WHEN apgs.rental_plan_price_12_months LIKE '%,%'
+         THEN SPLIT_PART(apgs.rental_plan_price_12_months, ',', 2)::FLOAT
+         ELSE apgs.rental_plan_price_12_months::FLOAT
+    END) AS avg_m12_high_price
+from dwh.product_reporting dwh
+left join pricing.all_pricing_grover_snapshots apgs
+on apgs.product_sku = dwh.product_sku and dwh.fact_day = apgs.snapshot_date
+where fact_day between '2023-11-01' and '2023-11-07' and store_id != 621
+group by dwh.product_sku
