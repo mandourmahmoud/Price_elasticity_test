74519a559cc852d76dc065156df7d97c8dba2cea
Mahmoud Mandour <mahmoudmandour@Mahmoud.Mandour MacBookPro>
2023-11-22 17:37:00 +0200

    Updating correct actual data query v2

--- acutal_data_script.sql
+++ acutal_data_script.sql
@@ -11,7 +11,9 @@ select  dwh.product_sku
          ELSE apgs.rental_plan_price_12_months::FLOAT
     END) AS avg_m12_high_price
 from dwh.product_reporting dwh
+join pricing.store_mapper_live_state sm
+on dwh.store_id = sm.store_id
 left join pricing.all_pricing_grover_snapshots apgs
-on apgs.product_sku = dwh.product_sku and dwh.fact_day = apgs.snapshot_date
-where fact_day between '2023-11-01' and '2023-11-07' and store_id != 621
+on apgs.product_sku = dwh.product_sku and dwh.fact_day = apgs.snapshot_date and apgs.store_parent = sm.store_code
+where fact_day between '2023-11-01' and '2023-11-07' and sm.store_code in ('de','es', 'nl', 'at') --and dwh.product_sku = 'GRB224P16219'
 group by dwh.product_sku
